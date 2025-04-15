fc54baae5a2be99e42c36f8414a7056816edfb3d
Serhii Rudenko <serhii.rudenko@prozorro.sale>
2025-04-15 08:59:08 +0300

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,11 +1,10 @@
-// build.gradle.kts
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
-//import org.jetbrains.kotlin.gradle.tasks.KotlinCompile // Імпорт може бути не обов'язковим
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    kotlin("jvm") version "2.0.0" // Використовуємо Kotlin 2.0.0
-    id("org.jetbrains.compose") version "1.6.10" // Використовуємо Compose, сумісний з Kotlin 2.0.0
-    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" // Додатковий плагін компілятора Compose для Kotlin 2.0.0+
+    kotlin("jvm") version "1.9.23" // Або 1.9.23
+    id("org.jetbrains.compose") version "1.6.10" // Або інша сумісна з Kotlin версією
+    id("org.jetbrains.kotlin.plugin.compose") version "2.0.0" // Версія = версії Kotlin
 }
 
 group = "com.example.kubemanager"
@@ -13,42 +12,31 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
-    maven("https://mvnrepository.com/artifact/io.kubernetes/client-java")
-    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev") // Репозиторій для Compose
+    maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
 }
 
-// Вказуємо Java Toolchain для узгодженості компіляції
 kotlin {
-    jvmToolchain(11) // Компілюємо під Java 11
+    jvmToolchain(11)
 }
 
 dependencies {
     implementation(compose.desktop.currentOs)
-    val clientJavaVersion = "20.0.1" // !!! ЗАМІНІТЬ НА АКТУАЛЬНУ СТАБІЛЬНУ ВЕРСІЮ !!!
-    implementation("io.kubernetes:client-java:${clientJavaVersion}")
-    //implementation("io.kubernetes:client-java-kubeconfig:${clientJavaVersion}")
-    //implementation("io.kubernetes:client-java-okhttp-helper:${clientJavaVersion}")
+    implementation("io.fabric8:kubernetes-client:6.13.5")
+
     implementation("ch.qos.logback:logback-classic:1.4.14")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-swing:1.8.0")
-    implementation("org.slf4j:slf4j-api:2.0.17")
     testImplementation(kotlin("test"))
 }
 
-
 compose.desktop {
     application {
-        mainClass = "MainKt" // Головний клас вашого додатку
-
+        mainClass = "MainKt"
         nativeDistributions {
-            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Rpm) // Формати для різних ОС
+            targetFormats(TargetFormat.Dmg, TargetFormat.Msi, TargetFormat.Deb, TargetFormat.Rpm)
             packageName = "KotlinKubeManager"
             packageVersion = "1.0.0"
-            // Налаштування для іконки, вендора тощо
-            // vendor = "YourCompany"
-            // description = "Simple Kubernetes Manager"
-            // copyright = "© 2025 YourCompany"
         }
     }
 }
