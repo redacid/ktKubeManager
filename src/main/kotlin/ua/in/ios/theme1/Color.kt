deb6105f43768c66464dea6ba116112376dbe8ec
Redacid <redacid@ios.in.ua>
2025-05-10 13:19:50 +0300

    ready containers view (#10)
    Co-authored-by: Serhii Rudenko <serhii.rudenko@prozorro.sale>

--- src/main/kotlin/ua/in/ios/theme1/Color.kt
+++ src/main/kotlin/ua/in/ios/theme1/Color.kt
@@ -2,5 +2,6 @@ package ua.`in`.ios.theme1
 
 import androidx.compose.ui.graphics.Color
 
-val CLR = java.awt.Color(23, 85, 183)
+val CLR = java.awt.Color(43, 45, 48)
 val SeedColor: Color = Color(CLR.red, CLR.green, CLR.blue)
+
